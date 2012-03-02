3184d42969768cda129e113eb3657d8f59924fd1
Hidekazu Tanaka <hidekazu.tanaka@gmail.com>
2012-03-02 22:15:17 +0900

    first commit

---
+++ src/main/java/org/holidayworking/WordCount.java
@@ -0,0 +1,44 @@
+package org.holidayworking;
+
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+public class WordCount extends Configured implements Tool {
+
+    @Override
+    public int run(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: JapaneseWordCount <input path> <output path>");
+            return -1;
+        }
+
+        Job job = new Job(getConf(), "JapaneseWordCount");
+        job.setJarByClass(WordCount.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.setMapperClass(WordCountMapper.class);
+        job.setReducerClass(WordCountReducer.class);
+
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputKeyClass(IntWritable.class);
+
+        return job.waitForCompletion(true) ? 0 : 1;
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.exit(ToolRunner.run(new WordCount(), args));
+    }
+
+}
---
+++ src/main/java/org/holidayworking/WordCountMapper.java
@@ -0,0 +1,19 @@
+package org.holidayworking;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        for (String token : value.toString().split(" ")) {
+            context.write(new Text(token), new IntWritable(1));
+        }
+    }
+
+}
